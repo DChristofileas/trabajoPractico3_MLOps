234015cf0780fd712beb28994224018df28ed2ff
DChristofileas <dimichristofileas@gmail.com>
2025-03-03 22:22:34 -0600

    Agregado endpoint /ping

--- main.py
+++ main.py
@@ -4,4 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "¡Hola! Esta es mi API con FastAPI y Docker"}
+    return {"message":"¡Hola! Esta es mi API con FastAPI y Docker"}
+@app.get("/ping")
+def ping():
+    return {"message": "pong"}
